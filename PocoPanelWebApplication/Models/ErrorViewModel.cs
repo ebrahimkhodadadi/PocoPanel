3d9ff3fe040d8d04f6714c7ac7d7191b7252023e
ebrahim <ebrahimkhodadadideveloper@gmail.com>
2022-02-12 22:07:10 +0330

    PocoPanelWebApplication

---
+++ PocoPanelWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PocoPanelWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
